552f674c4a33a5d70cf3990a9fcbbc9f5fea25c2
mrJeppard <duncmc831@gmail.com>
2017-03-17 15:44:52 -0700

    added needed indent

--- www/placeNode_web.py
+++ www/placeNode_web.py
@@ -187,46 +187,46 @@ def calcComplete(result, ctx, app):
     return result
 
 if __debug__:
-def calcTestStub(opts):
+    def calcTestStub(opts):
 
-    #print 'opts.newNodes', opts.newNodes
-    
-    if 'testError' in opts.newNodes:
-        return {
-            'error': 'Some error message or stack trace'
-        }
-    elif len(opts.newNodes) == 1:
-        return {'nodes': {
-            'newNode1': {
-                'x': 73,
-                'y': 91,
-                'neighbors': {
-                    'TCGA-BP-4790': 0.352,
-                    'TCGA-AK-3458': 0.742,
-                }
-            },
-        }}
-    elif len(opts.newNodes) > 1:
-        return {'nodes': {
-            'newNode1': {
-                'x': 73,
-                'y': 91,
-                'neighbors': {
-                    'TCGA-BP-4790': 0.352,
-                    'TCGA-AK-3458': 0.742,
-                }
-            },
-            'newNode2': {
-                'x': 53,
-                'y': 47,
-                'neighbors': {
-                    'neighbor1': 0.567,
-                    'neighbor2': 0.853,
-                }
-            },
-        }}
-    else:
-        return { 'error': 'unknown test' }
+        #print 'opts.newNodes', opts.newNodes
+
+        if 'testError' in opts.newNodes:
+            return {
+                'error': 'Some error message or stack trace'
+            }
+        elif len(opts.newNodes) == 1:
+            return {'nodes': {
+                'newNode1': {
+                    'x': 73,
+                    'y': 91,
+                    'neighbors': {
+                        'TCGA-BP-4790': 0.352,
+                        'TCGA-AK-3458': 0.742,
+                    }
+                },
+            }}
+        elif len(opts.newNodes) > 1:
+            return {'nodes': {
+                'newNode1': {
+                    'x': 73,
+                    'y': 91,
+                    'neighbors': {
+                        'TCGA-BP-4790': 0.352,
+                        'TCGA-AK-3458': 0.742,
+                    }
+                },
+                'newNode2': {
+                    'x': 53,
+                    'y': 47,
+                    'neighbors': {
+                        'neighbor1': 0.567,
+                        'neighbor2': 0.853,
+                    }
+                },
+            }}
+        else:
+            return { 'error': 'unknown test' }
 
 def calc(dataIn, ctx, app):
 
