b55a7782ac75f36cfcb10e5ffcfa442ea095a367
swat <swat@darla.local>
2017-09-27 09:41:54 -0700

    cleaned up run scripts

--- www/config/swat.sh
+++ www/config/swat.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-DRLPATH=/Users/swat/drl/drl-graph-layout/bin
 HTTP=1
 WWW_SOCKET=127.0.0.1:5000
+
+# These don't propagate to where they're needed, so put it in login profile
+#DRLPATH=/Users/swat/packages/drl-graph-layout/bin
+#export PATH=$DRLPATH:$PATH
+#export PYTHONPATH=$WWW_PATH:$WWW_PATH/../calc
 
 export DATA_ROOT=/Users/swat/data/
 export FLASK_DEBUG=1
 export DEBUG=1
 export UNIT_TEST=0
-export PATH=$DRLPATH:$PATH
-export PYTHONPATH=$WWW_PATH:$WWW_PATH/../calc
 export VIEWER_URL=http://localhost:3333
 export ALLOWABLE_VIEWERS=https://tumormap.ucsc.edu,https://hexdev.sdsc.edu,http://localhost:3333
 export ADMIN_EMAIL=hexmap@ucsc.edu
