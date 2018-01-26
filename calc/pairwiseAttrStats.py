951d642f8a33b51a6736effc35109deb3d736d3b
Duncan McColl <duncmc831@gmail.com>
2018-01-26 10:44:30 -0800

    clean up; re-org; re-name

---
+++ calc/pairwiseAttrStats.py
@@ -0,0 +1,285 @@
+#! /usr/bin/env python2.7
+"""
+Functions for the pairwise attribute stat tests done by tumormap.
+"""
+import pandas as pd
+import numpy as np
+import sklearn.metrics.pairwise as sklp
+from scipy import stats
+from leesL import getLayerIndex
+import statsmodels.sandbox.stats.multicomp as multicomp
+import math
+import multiprocessing
+
+def oneByAllStats(attrDF, datatypeDict, newAttr, newAttrDataType):
+    """
+    import pandas as pd
+    import numpy as np
+    attrDF = pd.read_table("/home/duncan/hex/compute/tests/out/stats/allAttributes.tab", index_col=0)
+    datatypeDict = read_data_types("/home/duncan/hex/compute/tests/out/stats/")
+    newAttr = attrDF[attrDF.columns[1]]
+    newAttrDataType = "bin"
+    :param attrDF: pandas dataframe, all attributes for a particular
+    map.
+    :param dataTypeDict: Datatype keys pointing to array of attrNames.
+    :param newLayer: Must be parallel to attrDF,
+    :param newLayerDataType:
+    :return:
+    """
+    # Filter down to only nodes that the new attr has.
+    attrDF = attrDF.loc[newAttr.index]
+
+    # Gather attribute names.
+    # This var contains the ordering that needs to be maintained
+    # in the processing pool.
+    attrNames = []
+    attrNames.extend(attrDF[datatypeDict['bin']].columns)
+    attrNames.extend(attrDF[datatypeDict['cat']].columns)
+    attrNames.extend(attrDF[datatypeDict['cont']].columns)
+    attrNames = pd.Series(attrNames)
+
+    # Separate the attributes out into data types.
+    binAtts = attrDF[datatypeDict['bin']]
+    catAtts = attrDF[datatypeDict['cat']]
+    contAtts = attrDF[datatypeDict['cont']]
+
+    # Fill with the Nan code (change this if you can!!!!!!!!!!!)
+    #binAtts = binAtts.fillna(BINCATNAN)
+    #catAtts = catAtts.fillna(BINCATNAN)
+    #contAtts = contAtts.fillna(FLOATNAN)
+
+    # Format DFs to create the pool for processing.
+    binAtts = binAtts.transpose().as_matrix()
+    catAtts = catAtts.transpose().as_matrix()
+    contAtts = contAtts.transpose().as_matrix()
+    attr = newAttr.as_matrix()
+
+    # Make the pool of operations for the new attribute depending on
+    # the dataType. The idea here is create a pool of triplets.
+    # We map a wrapper function through this pool that calls the
+    # function (first triplet) with the two args (2nd and 3rd triplet).
+    # Again, the ordering of the pool needs to be kept the same as the
+    # columnNames var, namely bin-cat-cont.
+    if newAttrDataType == "bin":
+        opPool = [(binBinTest, attr, np.array(x)) for x in binAtts]
+        opPool.extend([(catBinOrCatCatTest, x, attr) for x in catAtts])
+        opPool.extend([(binContTest, attr, x) for x in contAtts])
+
+    elif newAttrDataType == "cat":
+        opPool = [(catBinOrCatCatTest, attr, x) for x in binAtts]
+        opPool.extend([(catBinOrCatCatTest, attr, x) for x in catAtts])
+        opPool.extend([(catContTest, attr, x) for x in contAtts])
+
+    elif newAttrDataType == "cont":
+        opPool = [(binContTest, x, attr) for x in binAtts]
+        opPool.extend([(catContTest, x, attr) for x in catAtts])
+        opPool.extend([(contContTest, x, attr) for x in contAtts])
+
+    else:
+        raise ValueError("Invalid datatype: " + newAttrDataType)
+
+    # Execute computations. Go parallel if there are many attributes
+    # to process.
+    if enoughForParallelComp(len(attrNames)):
+        pool = multiprocessing.Pool(processes=5)
+        pvalues = pd.Series(pool.map(callFunc, opPool))
+        pool.close()
+        pool.join()
+    else:
+        pvalues = pd.Series(map(callFunc, opPool))
+
+    # Filter out pvalues with NaN because they futz with the
+    # pvalue adjustments.
+    validIndecies = np.isfinite(pvalues).values
+    filteredps = pvalues[validIndecies].as_matrix()
+    filteredNames = attrNames[validIndecies]
+
+    # Run the adjustments.
+    FdrBHs = multicomp.multipletests(
+        filteredps,
+        alpha=0.05,
+        method='fdr_bh'
+    )[1]
+    bonferonnis = multicomp.multipletests(
+        filteredps,
+        alpha=0.05,
+        method='bonferroni'
+    )[1]
+
+    # Construct DataFrame and return.
+    colnames = [
+        "single test pvalue",
+        "FDRBH",
+        "bonferonnis"
+    ]
+
+    dfDict = dict(
+        zip(
+            colnames,
+            [filteredps, FdrBHs, bonferonnis],
+        )
+    )
+
+    pValueDF = pd.DataFrame(
+        dfDict,
+        index=filteredNames,
+        columns=colnames
+    )
+
+    return pValueDF
+
+
+def read_matrices(projectDir):
+    '''
+    Puts the metadata matrices files in a list, to be used in
+    utils.getAttributes()
+     and is dependent upon the matrix file being in proper format.
+    @param projectDir: the project directory
+    @return: a list of the matrix file names
+    '''
+    matlist = []
+    #grab each name
+    for line in open(projectDir + '/matrices.tab'):
+        matlist.append(projectDir + '/' + line.strip())
+
+    return matlist
+
+
+def read_data_types(projectDir):
+    '''
+
+    @param projectDir:
+    @return:
+    '''
+    #mapping from what is in the file to abbreviations used in dataTypeDict
+    dtypemap = {"Continuous":"cont","Binary":"bin","Categorical":"cat"}
+    dtfin = open(projectDir + '/Layer_Data_Types.tab')
+    dataTypeDict = {}
+    for line in dtfin:
+        line = line.strip().split('\t')
+        #if we recognize the category
+        if line[0] in dtypemap:
+            try:
+                dataTypeDict[dtypemap[line[0]]] = line[1:]
+            except IndexError:
+                dataTypeDict[dtypemap[line[0]]] = []
+
+    return dataTypeDict
+
+
+def enoughForParallelComp(N):
+    ENOUGH = 500
+    return N >= ENOUGH
+
+
+def binBinTest(x,y):
+    x,y = filterNan(x,y)
+    table = contingencyTable(x,y)
+
+    # Make sure there are enough values in each slot of table.
+    if table.shape != (2,2):
+        return np.NAN
+    try:
+        oddsratio, pValue = stats.fisher_exact(table)
+    except ValueError:
+        pValue = np.nan
+
+    return pValue
+
+
+def catBinOrCatCatTest(x,y):
+    x,y = filterNan(x,y)
+    table = contingencyTable(x,y)
+    try:
+        chi2, pValue, dof, expectedFreq = stats.chi2_contingency(table)
+    except ValueError:
+        pValue = np.nan
+
+    return pValue
+
+
+def contContTest(x,y):
+    x,y = filterNan(x,y)
+    try:
+        correlation, pValue = stats.pearsonr(x,y)
+    except ValueError:
+        pValue = np.nan
+
+    return pValue
+
+
+def catContTest(catx, conty):
+
+    catx, conty = filterNan(catx,conty)
+
+    groups = pd.DataFrame([catx,conty]).transpose().groupby([0])
+
+    samples = groups.aggregate(lambda x: list(x))[1].tolist()
+    try:
+        stat, pValue = stats.mstats.kruskalwallis(*samples)
+    except ValueError:
+        pValue = np.nan
+
+    return pValue
+
+
+def binContTest(binx, conty):
+    binx, conty = filterNan(binx, conty)
+
+    # Make groups according to binary stat test.
+    groupDF = pd.DataFrame([binx, conty])
+    groups = groupDF.transpose().groupby(groupDF.index[0])
+
+    # Manipulate to format expected by stats.ranksums().
+    two_samples = groups.aggregate(lambda x: list(x)).values
+
+    # Compute stats.
+    try:
+        stat, pValue = stats.ranksums(*two_samples)
+    except ValueError:
+        pValue = np.nan
+    except TypeError:
+        # Happens when there are not two groups formed.
+        pValue = np.nan
+
+    return pValue
+
+
+def contingencyTable(x,y):
+    """
+    Make a contingency table.
+    :param x: discrete numpy array
+    :param y: discrete numpy array
+    :return: 2d numpy array: y's are the columns x's are the rows
+    """
+    x_n = len(set(x))
+    y_n = len(set(y))
+
+    # Variable combinations pointing to indecies where they occur.
+    groupDict = pd.DataFrame([x,y],index=[0,1]).transpose().groupby([0,1]).groups
+
+    # Represents all combinations of variables.
+    table = np.repeat(0, x_n*y_n).reshape(x_n, y_n)
+
+    # Fill each variable combo there is data for.
+    for key in groupDict.keys():
+        # Key is modified to prevent off by one error when accessing
+        modkey = tuple(map(lambda x: int(x)-1, key))
+
+        table[modkey] = len(groupDict[key])
+
+    return table
+
+
+def filterNan(x,y):
+    eitherNan = np.logical_or(np.isnan(x),np.isnan(y))
+    x=x[~eitherNan]
+    y=y[~eitherNan]
+    return x,y
+
+
+def callFunc(triplet):
+    f= triplet[0]
+    x =triplet[1]
+    y =triplet[2]
+    return f(x,y)
