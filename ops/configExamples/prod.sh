57c51a400ae87967cc91679f9344db7bca570503
“terraswat” <t@santacruzwood.com>
2018-07-23 14:01:08 -0700

    config for bop

--- ops/configExamples/prod.sh
+++ ops/configExamples/prod.sh
@@ -27,27 +27,27 @@ export DRLPATH=$ROOT/packages/drl-graph-layout/bin
 # Controls amount of chatter on server output.
 export FLASK_DEBUG=0
 
-# Username for checking existence of server processes.
+# Username under which the data server will run, for checking existence of
+server processes.
 export HEX_UID=hexmap
 
 # User group to own server processes on protected ports.
 export HEX_GID=protein
 
-# Path to the install tar file.
+# Path to the install tar file when doing upgrades from a development data server.
 export INSTALL_TAR_PATH=$TOOLS/dev/compute/ops/install.tgz
 
 # https private key
 export KEY=$CERTS/server.key
 
-# Only needed for some centos. Centos complains about missing a shared
-# library when calling uwsgi. The solution is to provide sym lynks
-# to make the file names conform. 
+# Only needed for some installations missing a shared library when calling
+# uwsgi. The solution is to provide sym links to make the file names conform.
 #export LD_LIBRARY_PATH=
 
 # Port on which the server will listen.
 export PORT=443
 
-# This path needs to be above/equal to your 'compute' dir in the
+# This path needs to be above or equal to your 'compute' dir in the
 # file hierarchy.
 export PYENV=$HEXCALC/../env
 
